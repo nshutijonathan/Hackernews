d42558151a53430ae3bb9c453d809ea2757427c4
nshutijonathan <nshutijonathan130@gmail.com>
2020-03-02 14:11:50 +0200

    initial commit

---
+++ links/schema.py
@@ -0,0 +1,45 @@
+import graphene
+from  graphene_django import DjangoObjectType
+from .models import Link
+from users.schema import UserType
+class LinkType(DjangoObjectType):
+    class Meta:
+        model=Link
+    
+class Query(graphene.ObjectType):
+    links=graphene.List(LinkType)
+
+    def resolve_links(self,info,**kwargs):
+        return Link.objects.all()
+
+    
+
+class CreateLink(graphene.Mutation):
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+    posted_by = graphene.Field(UserType)
+
+    class Arguments:
+        url = graphene.String()
+        description = graphene.String()
+
+    def mutate(self, info, url, description):
+        user = info.context.user
+        link = Link(
+            url=url,
+            description=description,
+            posted_by=user,
+        )
+        link.save()
+
+        return CreateLink(
+            id=link.id,
+            url=link.url,
+            description=link.description,
+            posted_by=link.posted_by,
+        )
+        
+class Mutation(graphene.ObjectType):
+    
+    create_link = CreateLink.Field()
